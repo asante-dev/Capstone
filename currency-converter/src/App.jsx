47cb6b7d1543e77e996345c05f31c7a9cce0d1b9
ChaseDev <clintonkwabena@gmail.com>
2025-08-16 17:50:01 +0000

    Capstone project setup

---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function App () {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Hello Currency Converter!</h1>
+    </div>
+  );
+}
+
+export default App;
